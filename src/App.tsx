519b3950cc510a30f1eb0a47e4d429a5430f60b9
NitinTSutar <nitintsuthar67@gmail.com>
2025-04-22 14:18:40 +0530

    created button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Button from "./Components/Button";
 import User from "./Components/User";
 
 const App = () => {
     return (
         <div>
-            <User name = "Nitin" age={24} inStudent={true}/>
+            <User name="Nitin" age={24} isStudent={true} />
+            <Button
+                label="Click"
+                onClick={() => alert('Button Clicked')}
+                disabled={false}
+            />
         </div>
     );
 };
--- src/Components/User.tsx
+++ src/Components/User.tsx
@@ -1,8 +1,20 @@
+import { FC } from "react";
 
-const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+type UserShape = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
 
-export default User
+
+const User:FC<UserShape> = ({ name, age, isStudent }) => {
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h1>{age}</h1>
+            <h1>{isStudent }</h1>
+        </div>
+    );
+};
+
+export default User;
