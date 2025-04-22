ab3117faa3d49a59647f284e99e8a207502f9202
NitinTSutar <nitintsuthar67@gmail.com>
2025-04-22 14:56:30 +0530

    Info module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import AdminInfo from "./Components/AdminInfo";
 import Button from "./Components/Button";
 import User from "./Components/User";
+import UserInfo from "./Components/UserInfo";
+import { Info, AdminInfoList } from "./types";
 
 const App = () => {
+
+    const user: Info = {
+        id: 1,
+        name: "Nitin Sutar",
+        email: "nitin@gmail.com"
+    }
+
+    const admin: AdminInfoList = {
+        id: 2,
+        name: "Admin Bhai",
+        email: "adminbhai@gmail.com",
+        role: "admin",
+        lastLogin: new Date(),  
+    }
+
     return (
         <div>
             <User name="Nitin" age={24} isStudent={true} />
@@ -10,6 +28,8 @@ const App = () => {
                 onClick={() => alert('Button Clicked')}
                 disabled={false}
             />
+            <UserInfo user={user} />
+            <AdminInfo admin={admin} />
         </div>
     );
 };
