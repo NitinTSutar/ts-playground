0d4a07d1e3a9705ec5e9f6ef1d35cff7e3850285
NitinTSutar <nitintsuthar67@gmail.com>
2025-04-22 13:43:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import User from "./Components/User";
+
+const App = () => {
+    return (
+        <div>
+            <User name = "Nitin" age={24} inStudent={true}/>
+        </div>
+    );
+};
+
+export default App;
